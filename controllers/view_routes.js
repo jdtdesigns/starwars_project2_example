3b5995acab35d2f3a090782a4fa087d6cdff45e2
JD Tadlock <jt7903@gmail.com>
2023-04-17 12:17:35 -0400

    seed added

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -3,7 +3,11 @@ const { Course } = require('../models');
 
 router.get('/', async (req, res) => {
   // Render Index Template and pass it a userName value with a student name
+  const courses = await Course.findAll({
+    raw: true
+  });
 
+  res.render('index', { courses });
 });
 
 
