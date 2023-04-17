e5c8f29c686be70dc8e12aa40c8211430b39c683
JD Tadlock <jt7903@gmail.com>
2023-04-17 14:35:17 -0400

    fin hbs basics

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -3,9 +3,22 @@ const { Course } = require('../models');
 
 router.get('/', async (req, res) => {
   // Render Index Template and pass it a userName value with a student name
-  res.render('index', {
-    userName: 'Eric'
+
+  // Get all courses, using the Course model and send the courses array to the index template
+  const courses = await Course.findAll({
+    raw: true
   });
+
+  res.render('index', {
+    courses: courses
+  });
+});
+
+router.post('/courses', async (req, res) => {
+  const course_data = req.body;
+  await Course.create(course_data);
+
+  res.redirect('/');
 });
 
 
